8143fdef2f03a4655ae9594a33ff9d37df1248bc
小丑皇 <84304741@qq.com>
2025-11-26 01:50:22 +0800

    去除导航

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,11 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         { <div className={styles.buttons}>
-          <Link
+          {/* <Link
             className="button button--secondary button--lg button--makai"
             to="/docs/intro">
             导航
-          </Link>
+          </Link> */}
         </div> }
       </div>
     </header>
