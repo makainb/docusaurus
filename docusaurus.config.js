f145560047463d15bc6a32865604bba04a78b5c7
小丑皇 <84304741@qq.com>
2025-12-27 00:38:52 +0800

    修复sidebar错误的问题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,7 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkBreaks from 'remark-breaks';
 
 
 
@@ -33,10 +28,6 @@ const config = {
 
   onBrokenLinks: 'throw',
 
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans'],
@@ -49,27 +40,25 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          remarkPlugins: [remarkMath],
+          remarkPlugins: [remarkMath, remarkBreaks],
           rehypePlugins: [rehypeKatex],
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: false,
+          remarkPlugins: [remarkMath, remarkBreaks],
+          rehypePlugins: [rehypeKatex],
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
+        },
+        // 3. 重点：在这里配置 src/pages 的 Markdown 插件
+        pages: {
+          remarkPlugins: [remarkMath, remarkBreaks],
+          rehypePlugins: [rehypeKatex],
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -90,25 +79,13 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: '藏鲸阁',
-        // logo: {
-        //   alt: '网站 Logo',
-        //   src: 'img/logo.jpg',
-        // },
         items: [
-          // {label: '文档',type: 'docSidebar',sidebarId: 'tutorialSidebar'},
-          // { label: '导航', sidebarId:'allSidebar', to: '/docs/intro',},
-          // { label: 'Java', sidebarId:'javaSidebar', to: '/docs/java/intro',},
-          // { label: '算法', to: '/docs/algorithm/intro',sidebarId:'algorithmSidebar',},
-          // { label: '工具', sidebarId:'toolsSidebar', to: '/docs/tools/jetbrains-idea',},
-          // { label: 'MOM文档', sidebarId:'momSidebar', to: '/docs/mom/use/base-tools',},
           {
             label:'Spring',
             type: 'dropdown',
-            sidevarId: 'devopsSidebar',
             items: [
 
               // 分隔符
@@ -117,72 +94,8 @@ const config = {
 
             ]
           },
-          // {
-          //   label:'DevOps',
-          //   type: 'dropdown',
-          //   sidevarId: 'devopsSidebar',
-          //   items: [
 
-          //     // 分隔符
-          //     { type: 'html', value: '<b>项目管理</b>', className: 'dropdown-archived-versions',},
-          //     { to: '/docs/devops/jira/intro', label: 'Jira',},
-
-          //     { type: 'html', value: '<b>文档管理</b>', className: 'dropdown-archived-versions',},
-          //     { to: '/docs/devops/confluence/intro', label: 'Confluence',},
-
-          //     { type: 'html', value: '<b>代码管理</b>', className: 'dropdown-archived-versions',},
-          //     // { to: '/about/todo', label: 'Gitlab',},
-          //     // { to: '/about/todo', label: 'Gitea',},
-
-          //     { type: 'html', value: '<b>制品管理</b>', className: 'dropdown-archived-versions',},
-          //     { to: '/docs/devops/nexus/intro', label: 'Nexus',},
-
-          //     // { type: 'html', value: '<b>持续集成</b>', className: 'dropdown-archived-versions',},
-          //     // { to: '/about/todo', label: 'Drone',},
-          //     // { to: '/about/todo', label: 'Jenkins',},
-          //     // { to: '/about/todo', label: 'Tekton',},
-
-          //     // { type: 'html', value: '<b>质量检查</b>', className: 'dropdown-archived-versions',},
-          //     // { to: '/about/todo', label: 'Sonarcube',},
-
-          //     // { type: 'html', value: '<b>持续部署</b>', className: 'dropdown-archived-versions',},
-          //     // { to: '/about/todo', label: 'ArgoCD',},
-          //     // { to: '/about/todo', label: 'KubeVela',},
-          //     // { to: '/about/todo', label: 'Zadig',},
-
-          //     { type: 'html', value: '<b>监控预警</b>', className: 'dropdown-archived-versions',},
-          //     { to: '/docs/devops/prometheus/install-docker-compose', label: 'Prometheus',},
-              
-          //   ],
-          // },
-
-
-          // { label: '1博客', position: 'right', to: '/blog'},
           { label: '博客', position: 'right', to: '/blog/archive',},
-
-
-          /*
-          {
-            type: 'dropdown',
-            label: 'Community',
-            position: 'left',
-            items: [
-              {
-                label: 'Facebook',
-                href: 'https://www.facebook.com',
-              },
-              {
-                // label: 'Facebook',
-                // href: 'https://www.facebook.com',
-                type: 'doc',
-                label: 'Social',
-                docId: 'java/intro',
-              },
-              // ... more items
-            ],
-          },
-
-          */
 
 
           {
@@ -193,11 +106,6 @@ const config = {
               // 分隔符
               { to: '/about/aboutme', label: '关于我',},
               { to: '/about/aboutsite', label: '关于本站', },
-
-              // { type: 'html', value: '<hr class="dropdown-separator">',},           
-              // { type: 'html', value: '<hr class="dropdown-separator">',},
-              // // 分组名
-              // { type: 'html', value: '<b>Archived versions</b>', className: 'dropdown-archived-versions',},
               
             ],
           },
@@ -206,57 +114,6 @@ const config = {
         ],
       },
 
-      /*
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      */
-      // prism: {
-      //   theme: prismThemes.github,
-      //   darkTheme: prismThemes.dracula,
-      // },
       prism: {
         theme: prismThemes.vsDark,
         magicComments: [
@@ -281,8 +138,6 @@ const config = {
             block: { start: 'highlight-error-start', end: 'highlight-error-end' }
           },
         ],
-        // languages enabled by default: https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L23
-        // prism supported languages: https://prismjs.com/#supported-languages
         additionalLanguages: [
           'java',
           'json',
